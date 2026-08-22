0a5a936f05d66675e91bd9220af7a620de60862d
Roshan9623 <dhangarroshan321@gmail.com>
2026-08-22 22:41:53 +0530

    Add first Java programs

---
+++ JF.java
@@ -0,0 +1,10 @@
+
+
+
+public class JF
+{
+	public static void main(String x[])
+	{
+		System.out.println("Good Mornimg");
+	}
+}
---
+++ JavaAPP.java
@@ -0,0 +1,10 @@
+
+
+
+
+public class JavaAPP{
+	
+	public static void main(String x[]){
+		System.out.println("Hello World");
+	}
+}
