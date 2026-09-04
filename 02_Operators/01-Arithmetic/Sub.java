84b54a2a7d28cf075519d951ce6fe71870f3e0b9
Roshan9623 <dhangarroshan321@gmail.com>
2026-09-04 22:09:40 +0530

    added new Sub.java

---
+++ 02_Operators/01-Arithmetic/Sub.java
@@ -0,0 +1,11 @@
+public class Sub
+{
+	public static void main(String x[])
+	{
+		int a =25,b = 15, c;
+		c = a - b;
+			
+		System.out.printf(" a = %d b = %d \n",a,b);
+		System.out.printf(" sub is %d ",c);
+	}
+}	 
