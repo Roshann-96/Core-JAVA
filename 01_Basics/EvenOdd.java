a94581de5d0f81fe4057638507588759e7db3a5f
Roshan9623 <dhangarroshan321@gmail.com>
2026-09-06 23:44:49 +0530

    added EvenOdd.java

---
+++ 01_Basics/EvenOdd.java
@@ -0,0 +1,18 @@
+public class EvenOdd
+{
+	public static void main(String x[])
+	{
+		int num = 12;
+		
+		if( num % 2 == 0 ){
+		
+			System.out.println( "num is Even");
+		}
+		else{
+		
+			System.out.println(" num is Odd");
+			
+		}
+	}
+}	
+			
