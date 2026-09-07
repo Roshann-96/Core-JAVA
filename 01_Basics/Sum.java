1e681b21c8097aac718f77a6c3721a293fd68f3a
Roshan9623 <dhangarroshan321@gmail.com>
2026-09-07 22:50:25 +0530

    added Sum.java

---
+++ 01_Basics/Sum.java
@@ -0,0 +1,13 @@
+public class Sum
+{
+	public static void main(String x[])
+	{
+		int a =Integer.parseInt(x[0]);
+		int b =Integer.parseInt(x[1]);
+		
+		int sum = a+b;
+		System.out.printf(" %d or %d \n",a,b);
+		System.out.printf(" sum is %d",sum);
+		
+	}
+}
