89f649b50ef005c3e6fb0d27b95c0c9f39441c4c
Roshan9623 <dhangarroshan321@gmail.com>
2026-09-08 22:49:54 +0530

    added MulP.java

---
+++ 02_Operators/01-Arithmetic/MulP.java
@@ -0,0 +1,13 @@
+
+public class MulP
+{
+	public static void main(String x[])
+	{
+		int a = 25;
+		int b = 5;
+		int c = a*b;
+		
+		System.out.printf(" a:%d or b:%d Multiply is %d",a,b,c);
+		
+	}
+}
