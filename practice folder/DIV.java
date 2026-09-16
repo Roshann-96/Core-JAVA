17d499132aa4b00f278e1be4a5b4a6b00a30e9bd
Roshan9623 <dhangarroshan321@gmail.com>
2026-09-16 22:04:06 +0530

    added DIV.java

---
+++ practice folder/DIV.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class DIV
+{
+	public static void main(String x[])
+	{
+		Scanner xyz = new Scanner(System.in);
+		
+		int a, b ,div;
+		
+		System.out.println("Enter 1st value: ");
+		a = xyz.nextInt();
+		
+		System.out.println("Enter 2nd value: ");
+		b = xyz.nextInt();
+		
+		div = a / b;
+		
+		System.out.printf("%d or %d  Div is : %d   ",a ,b, div);
+	}
+}
