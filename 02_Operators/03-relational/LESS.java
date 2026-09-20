9a89fbad46c142c5119d762b2427a7cde74406ec
Roshan9623 <dhangarroshan321@gmail.com>
2026-09-20 15:23:58 +0530

    Added LESS.java

---
+++ 02_Operators/03-relational/LESS.java
@@ -0,0 +1,13 @@
+public class LESS
+{
+	public static void main(String x[])
+	{
+		int a = 11;
+		int b = 10;
+		
+		boolean result = a > b;
+		
+		System.out.printf("Result: %b",result);
+		
+	}
+}
