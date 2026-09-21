c455f6afd33afcf5b8e5b25c0129d4af59c79e43
Roshan9623 <dhangarroshan321@gmail.com>
2026-09-21 14:10:49 +0530

    Added GREATER.java

---
+++ 02_Operators/03-relational/GREATER.java
@@ -0,0 +1,13 @@
+public class GREATER
+{
+	public static void main(String x[])
+	{
+		int n = 20;
+		int c = 34;
+		
+		boolean result = c > n;
+		
+		System.out.printf("Result :- %b",result);
+		
+	}
+}
